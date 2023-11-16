017b86de82b47db1317bea5ab431459306d20530
Renato Rodrigues <renato.rojunior@gmail.com>
2023-11-16 09:35:55 -0300

    Criando as tabelas.

--- connection.php
+++ connection.php
@@ -1,10 +1,38 @@
 <?php
 
-// use PDO;
-
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
 echo 'Conectado';
+
+$createTableSql = '
+    CREATE TABLE IF NOT EXISTS clients (
+        id INTEGER PRIMARY KEY,
+        cpf TEXT,
+        client_name TEXT,
+        email TEXT,
+        cellphone TEXT,
+        address TEXT
+    );    
+
+    CREATE TABLE IF NOT EXISTS products (
+        id INTEGER PRIMARY KEY,
+        product_code TEXT,
+        product_name TEXT,
+        price TEXT,
+        stock_quantity INTEGER        
+    );
+
+    CREATE TABLE IF NOT EXISTS orders (
+        id INTEGER PRIMARY KEY,
+        value TEXT,
+        client_id INTEGER, 
+        product_id INTEGER,        
+        FOREIGN KEY(client_id) REFERENCES clients(id),
+        FOREIGN KEY(product_id) REFERENCES products(id)
+    );
+';
+
+$pdo->exec($createTableSql);
 
 ?>
